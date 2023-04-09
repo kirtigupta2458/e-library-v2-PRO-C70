ac6bcc11dea22e385250cc79c07dd02ded71e006
kirtigupta2458 <72331388+kirtigupta2458@users.noreply.github.com>
2023-04-09 21:27:29 +0530

    Update Transaction.js

--- screens/Transaction.js
+++ screens/Transaction.js
@@ -70,42 +70,19 @@ export default class TransactionScreen extends Component {
     }
     return (
       <View style={styles.container}>
-        <ImageBackground source={bgImage} style={styles.bgImage}>
+        
           <View style={styles.upperContainer}>
-            <Image source={appIcon} style={styles.appIcon} />
-            <Image source={appName} style={styles.appName} />
+            
           </View>
           <View style={styles.lowerContainer}>
             <View style={styles.textinputContainer}>
-              <TextInput
-                style={styles.textinput}
-                placeholder={"Book Id"}
-                placeholderTextColor={"#FFFFFF"}
-                value={bookId}
-              />
-              <TouchableOpacity
-                style={styles.scanbutton}
-                onPress={() => this.getCameraPermissions("bookId")}
-              >
-                <Text style={styles.scanbuttonText}>Scan</Text>
-              </TouchableOpacity>
+             
             </View>
             <View style={[styles.textinputContainer, { marginTop: 25 }]}>
-              <TextInput
-                style={styles.textinput}
-                placeholder={"Student Id"}
-                placeholderTextColor={"#FFFFFF"}
-                value={studentId}
-              />
-              <TouchableOpacity
-                style={styles.scanbutton}
-                onPress={() => this.getCameraPermissions("studentId")}
-              >
-                <Text style={styles.scanbuttonText}>Scan</Text>
-              </TouchableOpacity>
+              
             </View>
           </View>
-        </ImageBackground>
+        
       </View>
     );
   }
